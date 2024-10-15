f6484967639df1b2b3d400d15ad6f28d91edc550
Tomislav Ogrinec <tomislav.ogrinec@factoryxhrvatska.onmicrosoft.com>
2024-10-15 14:08:53 +0200

    Refactor code

---
+++ FOI_labos_Blazor/Configuration/ServiceCollections.cs
@@ -0,0 +1,21 @@
+﻿using FOI_labos_Blazor.Data;
+using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
+
+namespace FOI_labos_Blazor.Configuration;
+
+public static class ServiceCollections
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMudServices();
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddRazorComponents()
+            .AddInteractiveWebAssemblyComponents();
+
+        return services;
+    }
+}
---
+++ FOI_labos_Blazor/Configuration/WebApplicationExstensions.cs
@@ -0,0 +1,51 @@
+﻿using FOI_labos_Blazor.Components;
+using FOI_labos_Blazor.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FOI_labos_Blazor.Configuration;
+
+public static class WebApplicationExstensions
+{
+    public static WebApplication Configure(this WebApplication app, IWebHostEnvironment environment,
+        IConfiguration configuration)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveWebAssemblyRenderMode()
+            .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+        return app;
+    }
+
+    /// <summary>
+    /// Apply migrations - kako bismo imali pripremljenu bazu sa svim potrebnim tablicama
+    /// </summary>
+    /// <param name="app"></param>
+    /// <returns></returns>
+    public static WebApplication MigrateDatabase(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        context.Database.Migrate();
+
+        return app;
+    }
+}
